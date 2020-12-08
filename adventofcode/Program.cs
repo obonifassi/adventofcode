8f899b349d2bab7359ccef45ba1fc524d7c7e43d
oscar bonifassi <oscar.bonifassi@gmail.com>
2020-12-07 21:33:47 -0800

    -implement day 1 and include solution file

---
+++ adventofcode/Program.cs
@@ -0,0 +1,17 @@
+﻿using adventofcode._2020;
+using System;
+
+namespace adventofcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day1Result = Day1.find2Sum();
+            Console.WriteLine("day 1a: " + day1Result);
+
+            day1Result = Day1.find3Sum();
+            Console.WriteLine("day 1b: " + day1Result);
+        }
+    }
+}
