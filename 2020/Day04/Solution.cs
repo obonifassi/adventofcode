a615f2a74a7b412174d8af288db78bb63b6ad2cc
oscar bonifassi <oscar.bonifassi@gmail.com>
2020-12-15 00:19:34 -0800

    add comment

--- 2020/Day04/Solution.cs
+++ 2020/Day04/Solution.cs
@@ -29,6 +29,7 @@ namespace adventofcode.Y2020.Day04
         {
             yield return PartOne(input);
             //
+            //
         }
 
         int PartOne(string input) => ValidCount(input, (map) =>
