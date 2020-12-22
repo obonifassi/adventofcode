4e243fbed4a67744b77275b05d4d4bae7ed61c1a
oscar bonifassi <oscar.bonifassi@gmail.com>
2020-12-22 01:04:21 -0800

    implement day 6 part 1

---
+++ HashSetExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+
+namespace adventofcode
+{
+    public static class HashSetExtensions
+    {
+        public static HashSet<T> Union<T>(this HashSet<T> self, HashSet<T> other)
+        {
+            var set = new HashSet<T>(self); // don't change the original set
+            set.UnionWith(other);
+            return set;
+        }
+    }
+}
