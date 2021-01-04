f0e4cba4b266f753c2c8e517b5e646f11358b1ed
oscar bonifassi <oscar.bonifassi@gmail.com>
2021-01-03 23:55:05 -0800

    finished implementing ft-day11 part one

--- 2020/Day11/Solution.cs
+++ 2020/Day11/Solution.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.ComponentModel;
 using System.Linq;
 
@@ -21,72 +20,94 @@ namespace adventofcode.Y2020.Day11
         long PartOne(string input)
         {
             var inputItems = input.Split("\r\n").Select(x => x.ToCharArray()).ToArray();
-            var items = inputItems.Select(a => a.ToArray()).ToArray();
+            var currentIteration = inputItems.Select(a => a.ToArray()).ToArray();
 
-            while (true)
+            bool isStable = false;
+            var debugRunningCounter = 0;
+            while (!isStable)
             {
-                char[][] itemsToUpdate = new char[items.Length][];
-
-                for (int i = 0; i <= items.Length - 1; i++)
-                {
-                    var row = items[i];
-
-                    char[] rowToUpdate = new char[row.Length];
-
-                    for (int j = 0; j <= row.Length - 1; j++)
-                    {
-                        bool IsValid = true;
-
-                        if (row[j] == 'L')
-                        {
-                            rowToUpdate[j] = 'L';
-
-                            IsValid = CheckAdjacentSeats(items, i, j, '#');
-
-                            if (IsValid)
-                            {
-                                rowToUpdate[j] = '#'; //seat becomes occupied
-                            }
-                        }
-                        else if (row[j] == '#')
-                        {
-                            rowToUpdate[j] = '#';
-
-                            IsValid = CheckAdjacentSeats(items, i, j, '#');
-
-                            if (IsValid)
-                            {
-                                rowToUpdate[j] = 'L'; //seat becomes empty
-                            }
-                        }
-                        else
-                        {
-                            rowToUpdate[j] = '.';
-                        }
-                    }
-
-                    itemsToUpdate[i] = rowToUpdate;
-                }
-
-                if(items.SequenceEqual(itemsToUpdate))
-                {
-                    break;
-                }
-
-                items = itemsToUpdate.Select(a => a.ToArray()).ToArray();
+                var (nextIteration, stable) = GetNextIteration(currentIteration);
+                isStable = stable;
+                currentIteration = nextIteration;
+                debugRunningCounter++;
             }
 
-            return -1;
+            //determine final count
+            var seatOccupiedCount = 0;
+            for (int i = 0; i < currentIteration.Length; i++)
+            {
+                for (int j = 0; j < currentIteration[i].Length; j++)
+                {
+                    if (currentIteration[i][j] == '#')
+                    {
+                        seatOccupiedCount++;
+                    }
+                }
+            }
+
+            return seatOccupiedCount;
         }
-        
-        bool CheckAdjacentSeats(char[][] items, int i, int j, char characterToCheck)
+
+        (char[][], bool) GetNextIteration(char[][] items)
+        {
+            bool IsStable = true;
+
+            char[][] itemsToUpdate = new char[items.Length][];
+
+            for (int i = 0; i <= items.Length - 1; i++)
+            {
+                var row = items[i];
+
+                char[] rowToUpdate = new char[row.Length];
+
+                for (int j = 0; j <= row.Length - 1; j++)
+                {
+                    bool IsValid;
+
+                    if (row[j] == 'L')
+                    {
+                        rowToUpdate[j] = 'L';
+
+                        IsValid = CheckAdjacentSeats(items, i, j, '#');
+
+                        if (IsValid)
+                        {
+                            rowToUpdate[j] = '#'; //seat becomes occupied
+                            IsStable = false; //we know we are not stable, when a change was required
+                        }
+                    }
+                    else if (row[j] == '#')
+                    {
+                        rowToUpdate[j] = '#';
+
+                        IsValid = CheckAdjacentSeats(items, i, j, '#', true);
+
+                        if (IsValid)
+                        {
+                            rowToUpdate[j] = 'L'; //seat becomes empty
+                            IsStable = false; //we know we are not stable, when a change was required
+                        }
+                    }
+                    else
+                    {
+                        rowToUpdate[j] = '.';
+                    }
+                }
+
+                itemsToUpdate[i] = rowToUpdate;
+            }
+
+            return (itemsToUpdate, IsStable);
+        }
+
+        bool CheckAdjacentSeats(char[][] items, int i, int j, char characterToCheck, bool checkRunningCounter = false)
         {
             bool IsValid = true;
             int runningCounter = 0;
             var row = items[i];
 
             //check right
-            if ((j + 1 < row.Length - 1) && row[j + 1] == characterToCheck)
+            if ((j + 1 <= row.Length - 1) && row[j + 1] == characterToCheck)
             {
                 IsValid = false;
                 runningCounter++;
@@ -100,7 +121,7 @@ namespace adventofcode.Y2020.Day11
             }
 
             //check down
-            if ((i + 1 < items.Length - 1) && items[i + 1][j] == characterToCheck)
+            if ((i + 1 <= items.Length - 1) && items[i + 1][j] == characterToCheck)
             {
                 IsValid = false;
                 runningCounter++;
@@ -114,21 +135,21 @@ namespace adventofcode.Y2020.Day11
             }
 
             //check right diagonal
-            if ((i - 1 >= 0) && (j + 1 < row.Length - 1) && items[i - 1][j + 1] == characterToCheck)
+            if ((i - 1 >= 0) && (j + 1 <= row.Length - 1) && items[i - 1][j + 1] == characterToCheck)
             {
                 IsValid = false;
                 runningCounter++;
             }
 
             //check left diagonal
-            if ((i + 1 < items.Length - 1) && (j - 1 >= 0) && items[i + 1][j - 1] == characterToCheck)
+            if ((i + 1 <= items.Length - 1) && (j - 1 >= 0) && items[i + 1][j - 1] == characterToCheck)
             {
                 IsValid = false;
                 runningCounter++;
             }
 
             //check right reverse diagonal
-            if ((i + 1 < items.Length - 1) && (j + 1 < row.Length - 1) && items[i + 1][j + 1] == characterToCheck)
+            if ((i + 1 <= items.Length - 1) && (j + 1 <= row.Length - 1) && items[i + 1][j + 1] == characterToCheck)
             {
                 IsValid = false;
                 runningCounter++;
@@ -141,7 +162,14 @@ namespace adventofcode.Y2020.Day11
                 runningCounter++;
             }
 
-            return IsValid || (runningCounter >= 4);
+            if (checkRunningCounter)
+            {
+                return (runningCounter >= 4);
+            }
+            else
+            {
+                return IsValid;
+            }
         }
         long PartTwo(string input)
         {
