5f94446e2871aeb84b697e0407a43a3cdc1c3515
oscar bonifassi <oscar.bonifassi@gmail.com>
2021-01-20 00:05:39 -0800

    implement day 12

---
+++ Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace adventofcode
+{
+    public static class Extensions
+    {
+        public static int Mod(this int a, int n)
+        {
+            a %= n;
+
+            if (a < 0)
+            {
+                return n + a;
+            }
+            else
+            {
+                return 0 + a;
+            }
+        }
+    }
+}
