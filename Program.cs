ce90dea2dd5afd53d573e44bc9980884b76a1dce
oscar bonifassi <oscar.bonifassi@gmail.com>
2022-06-12 21:10:19 -0700

    update to .net 6

--- Program.cs
+++ Program.cs
@@ -1,26 +1,18 @@
-﻿using System;
+﻿using adventofcode;
+using System;
 using System.Linq;
 using System.Reflection;
 
-namespace adventofcode
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var allValidTypes = Assembly.GetEntryAssembly()!.GetTypes()
+var allValidTypes = Assembly.GetEntryAssembly()!.GetTypes()
                 .Where(t => t.GetTypeInfo().IsClass && typeof(ISolver).IsAssignableFrom(t))
                 .OrderBy(t => t.FullName)
                 .ToArray();
 
-            var solutions = allValidTypes
-                            .Select(t => Activator.CreateInstance(t) as ISolver)
-                            .ToArray();
+var solutions = allValidTypes
+                .Select(t => Activator.CreateInstance(t) as ISolver)
+                .ToArray();
 
-            foreach (var s in solutions)
-            {
-                Orchestrator.RunSolver(s);
-            }
-        }
-    }
+foreach (var s in solutions)
+{
+    Orchestrator.RunSolver(s);
 }
